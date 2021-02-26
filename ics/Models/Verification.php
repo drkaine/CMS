6f3d7ee375ead7aeae5ded53faa1c645b28672da
drkaine <duretkevin@live.fr>
2021-02-26 10:58:46 +0100

    Add files via upload

--- ics/Models/Verification.php
+++ ics/Models/Verification.php
@@ -2,15 +2,115 @@
 
 	class Verificattion
 	{
-
-		function securite_Fiche()
+		///// secu formulaire ajout utilisateur ///////
+		static function verificationIdentifiant($P)
 		{
+			$erreurs = null;
+			if(!empty($P["mail"]))
+			{
+				if(!filter_var($P["mail"],FILTER_VALIDATE_EMAIL))
+				{
+					$erreurs[] = "InvalidateEmail";
+				}
+			}
+			else if(empty($P["Nom"]))
+			{
+				$erreurs[] = "NomNull";
+			}
+			else if(empty($P["Prenom"]))
+			{
+				$erreurs[] = "PrenomNull";
+			}
+			else if(empty($P["Mail"]))
+			{
+				$erreurs[] = "MailNull";
+			}
+			else if(empty($P["Mot_de_passe"]))
+			{
+				$erreurs[] = "MDPNull";
+			}
+			else if(strlen($P["Mot_de_passe"])>10)
+			{
+				//$erreurs[] = "MDPLong";
+				echo 'Votre mot de passe est trop long';
+			}
 
+
+			if($erreurs != null)
+			{
+				$post = array();
+        		foreach($P as $key => $value){
+		            $post[] = $key."=".$value;
+		        }
+		        header("Location: index.php?page=inscription&erreurs=".implode("--", $erreurs)."&".implode("&", $post));
+		        exit;
+			}
+			else
+			{
+				if (User::createUser($P))
+				{
+					User::createUser($P);
+					header("Location: index.php?page=inscriptionreussi");
+					exit;
+				}
+				else
+				{
+					header("Location : index.php?page=inscription&erreur=nonEnregistre");
+				}
+			}
 		}
 
-		function securite_Utilisateur()
+		///// secu formulaire ajout fiches métiers ///////
+		static function verificationMetier($P)
 		{
+			$erreurs = null;
+			if(!empty($P["titre"]))
+			{
+				if(empty($P["titre"]))
+				{
+					$erreurs[] = "Invalidatetitre";
+				}
+			}
+			else if(empty($P["rom"]))
+			{
+				$erreurs[] = "romNull";
+			}
+			else if(strlen($P["rom"])>5)
+			{
+				
+				echo 'Votre code ROM est trop long';
+			}
+			else if(empty($P["descriptionc"]))
+			{
+				$erreurs[] = "DescriptioncNull";
+			}
+			else if(empty($P["descriptiond"]))
+			{
+				$erreurs[] = "descriptiondNull";
+			}
 
+			if($erreurs != null)
+			{
+				$post = array();
+				foreach($P as $key => $value){
+					$post[] = $key."=".$value;
+				}
+				header("Location: index.php?page=inscription&erreurs=".implode("--", $erreurs)."&".implode("&", $post));
+				exit;
+			}
+			else
+			{		
+				if (Database::creation_Fiche($P))
+				{
+					Database::creation_Fiche($P);
+					header("Location: index.php?page=inscriptionreussi");
+					exit;
+				}
+				else
+				{
+					header("Location : index.php?page=inscription&erreur=nonEnregistre");
+				}
+			}
 		}
 
 	}
